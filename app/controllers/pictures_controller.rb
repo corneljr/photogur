2c3637e7c5be4af58fad3d19f6694d7f7fcc942b
Josh Cornelius <joshcornelius9@gmail.com>
2014-06-16 18:20:18 -0400

    moving pictures into database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,31 +4,13 @@ class PicturesController < ApplicationController
 	end
 
 	def show
-			@pictures = [{
-				:title => "Europe",
-				:artist => "Josh Cornelius",
-				:url => '019.JPG'
-			},
-			{
-				:title => "Europe Again!",
-				:artist => "Josh Cornelius",
-				:url => '031.JPG'
-			},
-			{
-				:title => "Europe one more time!",
-				:artist => "Josh Cornelius",
-				:url => '316.JPG'
-			}
-		]
-
-		@pictures = @pictures[params[:id].to_i]
+		@pictures = Pictures.find(params[:id])
 	end
 
 	def new
 	end
 
 	def create
-
 	end
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Picture.create!(
+	:title => "Europe",
+	:artist => "Josh Cornelius",
+	:url => '019.JPG'
+)
+
+Picture.create!(
+	:title => "Europe Again!",
+	:artist => "Josh Cornelius",
+	:url => '031.JPG'
+)
+
+Picture.create!(
+	:title => "Europe one more time!",
+	:artist => "Josh Cornelius",
+	:url => '316.JPG'
+)
