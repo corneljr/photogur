6c6817f52befff04e0d3e69bf3dec8c41a455cb5
Josh Cornelius <joshcornelius9@gmail.com>
2014-06-18 13:59:10 -0400

    changed heroku default url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,5 +26,5 @@ Photogur::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'http://shielded-reaches-6577.herokuapp.com/' }
 end
